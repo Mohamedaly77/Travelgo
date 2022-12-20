ba1a5d3b291c18588b0906bc912d366d2be6cb64
Mohamed Aly <mmkamel77@gmail.com>
2022-12-20 20:38:15 +0200

    Updates

---
+++ precache-manifest.48712ed26bdadb06d832223a9568449e.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "05a25ac01a48363317e3",
+    "url": "/Travelgo/static/css/main.651332ab.chunk.css"
+  },
+  {
+    "revision": "05a25ac01a48363317e3",
+    "url": "/Travelgo/static/js/main.2b54aa5e.chunk.js"
+  },
+  {
+    "revision": "2ab59f8db3dd36d318db",
+    "url": "/Travelgo/static/js/runtime~main.fc798f4c.js"
+  },
+  {
+    "revision": "43e919fd9ef5747adcb3",
+    "url": "/Travelgo/static/css/2.f822bd47.chunk.css"
+  },
+  {
+    "revision": "43e919fd9ef5747adcb3",
+    "url": "/Travelgo/static/js/2.0aeaf38f.chunk.js"
+  },
+  {
+    "revision": "8163f13eb499f5c0d529",
+    "url": "/Travelgo/static/js/3.8d74271d.chunk.js"
+  },
+  {
+    "revision": "7d9758b8bafaa4987024955fe85edad2",
+    "url": "/Travelgo/static/media/cover.7d9758b8.png"
+  },
+  {
+    "revision": "d3327ae3357bbf22b1b535c91a94d53a",
+    "url": "/Travelgo/static/media/luxor.d3327ae3.jpg"
+  },
+  {
+    "revision": "fc29574b120ca63aa0e407c8ff7a1b82",
+    "url": "/Travelgo/static/media/greece.fc29574b.jpg"
+  },
+  {
+    "revision": "638dadc40458a1e8f617407460b93d56",
+    "url": "/Travelgo/static/media/newyork.638dadc4.jpg"
+  },
+  {
+    "revision": "2b449e32bd363be455dbfac435ae8f97",
+    "url": "/Travelgo/static/media/subscribe.2b449e32.jpg"
+  },
+  {
+    "revision": "1324d9d6df874d3906100a730d134c34",
+    "url": "/Travelgo/static/media/membership.1324d9d6.jpg"
+  },
+  {
+    "revision": "7c56972f98d740d4a09517a36720312d",
+    "url": "/Travelgo/static/media/egypt1.7c56972f.jpg"
+  },
+  {
+    "revision": "b8e5957eef68152ea6bc8ede73ae7f1c",
+    "url": "/Travelgo/static/media/america.b8e5957e.jpg"
+  },
+  {
+    "revision": "51f0a4d7ae357f976fc96960cc4086de",
+    "url": "/Travelgo/static/media/acropolis.51f0a4d7.jpg"
+  },
+  {
+    "revision": "0efccc2f95bf61d15c4e5bbfec5ef0c0",
+    "url": "/Travelgo/static/media/suitcase.0efccc2f.png"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Travelgo/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Travelgo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Travelgo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Travelgo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Travelgo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "72d7a80a3a6a3fb54937c2e5c6093aca",
+    "url": "/Travelgo/static/media/mountaines.72d7a80a.jpg"
+  },
+  {
+    "revision": "6879eb103bfc047dd3146c045c7e6db1",
+    "url": "/Travelgo/index.html"
+  }
+];

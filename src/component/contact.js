b76f498a77dd8f6d98f336c8a6b57c975c359256
Mohamed Aly <mmkamel77@gmail.com>
2022-12-21 15:43:38 +0200

    Final Touch

--- src/component/contact.js
+++ src/component/contact.js
@@ -23,14 +23,14 @@ function Contact(props) {
             </div>
           </div>
 
-          <div className="col-md-6">
+          <div className="col-md-6 contact">
             <div className="mt-3">
               <div>
                 <h4 className="ms-4">Follow us </h4>
-                <i className="fa-brands fa-facebook m-1"></i>
-                <i className="fa-brands fa-instagram m-1"></i>
-                <i className="fa-brands fa-twitter m-1"></i>
-                <i className="fa-brands fa-youtube m-1"></i>
+                <i className="face fa-brands fa-facebook m-1"></i>
+                <i className=" insta fa-brands fa-instagram m-1"></i>
+                <i className=" twitter fa-brands fa-twitter m-1"></i>
+                <i className=" youtube fa-brands fa-youtube m-1"></i>
               </div>
             </div>
           </div>

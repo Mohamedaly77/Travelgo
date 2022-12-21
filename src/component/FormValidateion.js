4f5172671bb4ae6b04efe595bd741ce40cb202d4
Mohamed Aly <mmkamel77@gmail.com>
2022-12-21 14:20:18 +0200

    fix the forms

--- src/component/FormValidateion.js
+++ src/component/FormValidateion.js
@@ -1,24 +1,24 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-function FormValidateion(ValidateInfo) {
-  const [values, setValues] = useState({
-    email: "",
-    username: "",
-  });
+// function FormValidateion(ValidateInfo) {
+//   const [values, setValues] = useState({
+//     email: "",
+//     username: "",
+//   });
 
-  const [errors, setErrors] = useState({});
+//   const [errors, setErrors] = useState({});
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
 
-    setValues({ ...values, [name]: value });
-  };
+//     setValues({ ...values, [name]: value });
+//   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors(ValidateInfo(values));
-  };
-  return { handleChange, values, handleSubmit, errors };
-}
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     setErrors(ValidateInfo(values));
+//   };
+//   return { handleChange, values, handleSubmit, errors };
+// }
 
-export default FormValidateion;
+// export default FormValidateion;
--- src/component/Sineup.js
+++ src/component/Sineup.js
@@ -1,26 +1,93 @@
 import React from "react";
 import membership from "../images/membership.jpg";
+import * as Yup from "yup";
+
+import { useFormik } from "formik";
 
 function Sineup(props) {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .required("Name is required")
+        .max(15, "Maximum length is 15 or less"),
+
+      email: Yup.string().required("Email is required"),
+
+      password: Yup.string().required("Password is required"),
+    }),
+
+    onSubmit: (value) => {
+      console.log(formik.values);
+    },
+  });
+
   return (
     <div>
       <div className="container row  ">
         <div className="col-md-6">
-          <form>
+          <form onSubmit={formik.handleSubmit}>
             <div>
               <h1 className="text-center">Sine up</h1>
-              <label htmlFor="">Username</label>
-              <input type="text" className="form-control" />
+              <label className="m-2" htmlFor="">
+                Username
+              </label>
+              <input
+                onChange={formik.handleChange}
+                value={formik.values.name}
+                name="name"
+                id="name"
+                type="text"
+                className="form-control"
+              />
+              {formik.errors.name ? (
+                <p className="text-white p-2 bg-info">{formik.errors.name}</p>
+              ) : null}
             </div>
             <div>
-              <label htmlFor="">Email</label>
-              <input type="email" className="form-control" />
+              <label className="m-2" htmlFor="">
+                Email
+              </label>
+              <input
+                onChange={formik.handleChange}
+                name="email"
+                value={formik.values.email}
+                type="email"
+                className="form-control"
+              />
+              {formik.errors.email ? (
+                <p className="text-white p-2  bg-info ">
+                  {formik.errors.email}
+                </p>
+              ) : null}
             </div>
             <div>
-              <label htmlFor="">password</label>
-              <input type="password" className="form-control" />
+              <label className="m-2" htmlFor="">
+                Password
+              </label>
+              <input
+                onChange={formik.handleChange}
+                name="password"
+                id="password"
+                value={formik.values.password}
+                type="password"
+                className="form-control"
+              />
+              {formik.errors.password ? (
+                <p className="text-white p-2 bg-info ">
+                  {formik.errors.password}
+                </p>
+              ) : null}
             </div>
-            <button className="btn btn-primary m-2">Submit</button>
+
+            <button type="submit" className="btn btn-primary m-2">
+              Submit
+            </button>
           </form>
         </div>
         <div className=" image col-md-6 mt-3">
